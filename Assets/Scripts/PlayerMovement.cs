23055ac9509f90b6666c68479b0b448f80bb7d2b
izorget <izorgetprods@gmail.com>
2024-08-21 17:27:20 +0200

    shenanigans s1 ep10

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,15 +9,19 @@ public class PlayerMovement : MonoBehaviour
 
     public PlayerInput input;
     InputAction action;
-    [SerializeField] float playerSpeed = 5;
+    [SerializeField] float playerSpeed = 5f;
     public Vector2 playerRotation;
     GameObject playerCamera;
+    public float mouseSensitivity = 5f;
     void Start()
+
+        ///
     {
         input = GetComponent<PlayerInput>();
         action = input.actions.FindAction("Move");
         playerCamera = GameObject.FindGameObjectWithTag("MainCamera");
 
+       Cursor.lockState = CursorLockMode.Locked;  
     }
 
     // Update is called once per frame
@@ -30,10 +34,10 @@ public class PlayerMovement : MonoBehaviour
         Vector2 direction = action.ReadValue<Vector2>();
         transform.position += new Vector3(direction.x, 0, direction.y) * playerSpeed * Time.deltaTime;
 
-        playerRotation.x +=  Input.GetAxis("Mouse Y");
-        playerRotation.y += Input.GetAxis("Mouse X");
+        playerRotation.x += Input.GetAxis("Mouse Y"); ;
+        playerRotation.y += Input.GetAxis("Mouse X") ;
         transform.localRotation = Quaternion.Euler(0, playerRotation.y, 0);
-        playerCamera.transform.localRotation = Quaternion.Euler(-playerRotation.x, playerRotation.y, 0);
+        playerCamera.transform.localRotation = Quaternion.Euler( playerRotation.x, playerRotation.y, 0);
 
 
     }
