2ebe39b803c91068d7e5ebc93aeaa9f9fdf6998f
izorget <izorgetprods@gmail.com>
2024-09-05 23:57:15 +0200

    shenanigans s2 ep2

---
+++ Assets/followEyes.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followEyes : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject eyes;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = eyes.transform.position + new Vector3(0f, 0.27985f, 0f);
+      
+       
+
+    }
+}
