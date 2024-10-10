b3e30198b9ce85607d1740c52f94ced4b262ce42
izorget <izorgetprods@gmail.com>
2024-10-10 17:25:05 +0200

    animation

--- Assets/Scripts/FloatingUI.cs
+++ Assets/Scripts/FloatingUI.cs
@@ -1,67 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI; 
+using UnityEngine.UI;
 
 public class FloatingText : MonoBehaviour
 {
-  
+ 
     Transform mainCam;
     Transform unit;
     Transform worldSpaceCanvas;
 
     public Vector3 offset; 
-    public float visibilityDistance = 2.0f; 
-
-
+    public float visibilityDistance = 2.0f;  
     CanvasGroup textCanvasGroup; 
+    public Animator externalAnimator; 
 
     void Start()
     {
-     
+       
         mainCam = Camera.main.transform;
         unit = transform.parent; 
         worldSpaceCanvas = GameObject.FindGameObjectWithTag("InteractUI").transform; 
 
-        transform.SetParent(worldSpaceCanvas);  
 
-     
+        transform.SetParent(worldSpaceCanvas);
+
+   
         textCanvasGroup = GetComponent<CanvasGroup>();
-
-      
         if (textCanvasGroup == null)
         {
             textCanvasGroup = gameObject.AddComponent<CanvasGroup>();
+        }
+
+      
+        if (externalAnimator == null)
+        {
+            Debug.LogError("No Animator assigned to FloatingText script.");
         }
     }
 
     void Update()
     {
- 
+      
         float distanceToPlayer = Vector3.Distance(unit.position, mainCam.position);
 
+      
         if (distanceToPlayer <= visibilityDistance)
         {
-            textCanvasGroup.alpha = 1f;  
-            textCanvasGroup.interactable = true;  
-            textCanvasGroup.blocksRaycasts = true;  
+          
+            textCanvasGroup.alpha = 1f;
+            textCanvasGroup.interactable = true;
+            textCanvasGroup.blocksRaycasts = true;
+
+         
+            if (Input.GetKeyDown(KeyCode.E) && externalAnimator != null)
+            {
+             
+                externalAnimator.SetBool("isOpening", true);
+            }
         }
         else
         {
-            textCanvasGroup.alpha = 0f;  
-            textCanvasGroup.interactable = false;  
-            textCanvasGroup.blocksRaycasts = false;  
+          
+            textCanvasGroup.alpha = 0f;
+            textCanvasGroup.interactable = false;
+            textCanvasGroup.blocksRaycasts = false;
+
+          
         }
 
         transform.LookAt(mainCam);
 
-  
         transform.Rotate(180, 0, 0);
 
-      
         Vector3 desiredPosition = unit.position + offset;
-
-       
         transform.position = desiredPosition;
     }
 }
