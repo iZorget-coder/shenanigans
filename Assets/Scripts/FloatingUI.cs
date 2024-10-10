abb738778415085e86294e932470998db9feb31b
izorget <izorgetprods@gmail.com>
2024-10-10 17:53:14 +0200

    done finished

--- Assets/Scripts/FloatingUI.cs
+++ Assets/Scripts/FloatingUI.cs
@@ -3,31 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class FloatingText : MonoBehaviour
-{
+public class FloatingText : MonoBehaviour 
+{ 
 
     Transform mainCam;
     Transform unit;
     Transform worldSpaceCanvas;
 
-    public Vector3 offset;
-    public float visibilityDistance = 2.0f;
+    public Vector3 offset; 
+    public float visibilityDistance = 2.0f;  
 
-    CanvasGroup textCanvasGroup;
-    public Animator externalAnimator;
-
-
+    CanvasGroup textCanvasGroup; 
+    public Animator externalAnimator;  
+   
     private bool isDoorOpen = false;
 
     void Start()
     {
+   
         mainCam = Camera.main.transform;
-        unit = transform.parent;
-        worldSpaceCanvas = GameObject.FindGameObjectWithTag("InteractUI").transform;  // Find the canvas by tag
+        unit = transform.parent;  
+        worldSpaceCanvas = GameObject.FindGameObjectWithTag("InteractUI").transform; 
 
-
+       
         transform.SetParent(worldSpaceCanvas);
-
 
         textCanvasGroup = GetComponent<CanvasGroup>();
         if (textCanvasGroup == null)
@@ -35,70 +34,68 @@ public class FloatingText : MonoBehaviour
             textCanvasGroup = gameObject.AddComponent<CanvasGroup>();
         }
 
-
         if (externalAnimator == null)
         {
             Debug.LogError("No Animator assigned to FloatingText script.");
         }
 
-
+        
         if (externalAnimator != null)
         {
             externalAnimator.SetBool("isOpening", false);
             externalAnimator.SetBool("isClosing", false);
-            isDoorOpen = false;
+            isDoorOpen = false; 
         }
+    }
 
-        void Update()
+    void Update()
+    {
+      
+        float distanceToPlayer = Vector3.Distance(unit.position, mainCam.position);
+
+
+        if (distanceToPlayer <= visibilityDistance)
         {
+           
+            textCanvasGroup.alpha = 1f;
+            textCanvasGroup.interactable = true;
+            textCanvasGroup.blocksRaycasts = true;
 
-            float distanceToPlayer = Vector3.Distance(unit.position, mainCam.position);
-
-
-            if (distanceToPlayer <= visibilityDistance)
+       
+            if (Input.GetKeyDown(KeyCode.E) && externalAnimator != null)
             {
-
-                textCanvasGroup.alpha = 1f;
-                textCanvasGroup.interactable = true;
-                textCanvasGroup.blocksRaycasts = true;
-
-
-                if (Input.GetKeyDown(KeyCode.E) && externalAnimator != null)
+               
+                if (!isDoorOpen)
                 {
-
-                    if (!isDoorOpen)
-                    {
-
-                        externalAnimator.SetBool("isOpening", true);
-                        externalAnimator.SetBool("isClosing", false);
-                        isDoorOpen = true;
-                    }
-                    else
-                    {
-
-                        externalAnimator.SetBool("isOpening", false);
-                        externalAnimator.SetBool("isClosing", true);
-                        isDoorOpen = false;
-                    }
+                  
+                    externalAnimator.SetBool("isOpening", true);
+                    externalAnimator.SetBool("isClosing", false);
+                    isDoorOpen = true; 
+                }
+                else
+                {
+                    
+                    externalAnimator.SetBool("isOpening", false);
+                    externalAnimator.SetBool("isClosing", true);
+                    isDoorOpen = false;  
                 }
             }
-            else
-            {
-
-                textCanvasGroup.alpha = 0f;
-                textCanvasGroup.interactable = false;
-                textCanvasGroup.blocksRaycasts = false;
-            }
-
-
-            transform.LookAt(mainCam);
-
-
-            transform.Rotate(180, 0, 0);
-
-
-            Vector3 desiredPosition = unit.position + offset;
-            transform.position = desiredPosition;
         }
+        else
+        {
+           
+            textCanvasGroup.alpha = 0f;
+            textCanvasGroup.interactable = false;
+            textCanvasGroup.blocksRaycasts = false;
+        }
+
+      
+        transform.LookAt(mainCam);
+
+       
+        transform.Rotate(180, 0, 0);
+
+        Vector3 desiredPosition = unit.position + offset;
+        transform.position = desiredPosition;
     }
 }
