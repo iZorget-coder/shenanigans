93b70f9f31ad035af4b2aa10412af01f899408e1
izorget <izorgetprods@gmail.com>
2024-10-12 20:10:44 +0200

    torch UI

--- Assets/torch.cs
+++ Assets/torch.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class torch : MonoBehaviour
 {
@@ -13,6 +15,16 @@ public class torch : MonoBehaviour
     bool hasPickedUpTorch = false;
     private bool isTorchOn = false;
 
+    public float torchDuration = 10f;
+    public float cooldownDuration = 30f;
+    private float torchTimer = 0f;
+    private float cooldownTimer = 0f;
+
+    private bool isInCooldown = false;
+
+    public Slider torchSlider;
+    public TMP_Text torchHintText1, torchHintText2;
+
     void Start()
     {
         torchScrpt = GameObject.FindGameObjectWithTag("drawerUI").GetComponent<FloatingText>();
@@ -20,41 +32,122 @@ public class torch : MonoBehaviour
         player = GameObject.FindGameObjectWithTag("Player");
         playerCamera = GameObject.FindGameObjectWithTag("MainCamera");
         torchSlotON.SetActive(false);
+
+        torchSlider.maxValue = torchDuration;
+        torchSlider.value = torchDuration;
+        torchSlider.gameObject.SetActive(false);
+
+        torchHintText1.gameObject.SetActive(false);
+        torchHintText2.gameObject.SetActive(false);
     }
 
     void Update()
     {
+        if (isTorchOn)
+        {
+            HandleTorchUsage();
+        }
+
+        if (isInCooldown)
+        {
+            HandleCooldown();
+        }
+
         if (torchScrpt.isDrawerOpen && Input.GetKeyDown(KeyCode.C))
         {
             if (torchSlotOFF != null && !hasPickedUpTorch)
             {
-                torchSlotOFF.SetActive(true);
-                torchObject.transform.SetParent(playerCamera.transform);
-                torchObject.transform.localPosition = new Vector3(-1.042f, -0.673f, -0.385f);
-                torchObject.transform.localRotation = Quaternion.Euler(-64.679f, 119.838f, 605.537f);
-                hasPickedUpTorch = true;
-                torchLight.SetActive(false);
-                torchSlotOFF.SetActive(false);
-                torchSlotON.SetActive(true);
+                PickUpTorch();
             }
 
-            if (hasPickedUpTorch && !isTorchOn && Input.GetKeyDown(KeyCode.C))
+            if (hasPickedUpTorch && !isTorchOn && !isInCooldown && Input.GetKeyDown(KeyCode.C))
             {
-                torchScrpt.isDrawerOpen = true;
-                torchLight.SetActive(true);
-                isTorchOn = true;
-                torchSlotON.SetActive(true);
-                torchSlotOFF.SetActive(false);
-
+                TurnOnTorch();
             }
             else if (hasPickedUpTorch && isTorchOn && Input.GetKeyDown(KeyCode.C))
             {
-                torchScrpt.isDrawerOpen = true;
-                torchLight.SetActive(false);
-                isTorchOn = false;
-                torchSlotON.SetActive(false);
-                torchSlotOFF.SetActive(true);
+                TurnOffTorch();
             }
+        }
+
+        if (hasPickedUpTorch && !isTorchOn && !isInCooldown)
+        {
+            torchHintText1.gameObject.SetActive(true);
+            torchHintText2.gameObject.SetActive(true);
+        }
+        else
+        {
+            torchHintText1.gameObject.SetActive(false);
+            torchHintText2.gameObject.SetActive(false);
+        }
+    }
+
+    void PickUpTorch()
+    {
+        torchSlotOFF.SetActive(true);
+        torchObject.transform.SetParent(playerCamera.transform);
+        torchObject.transform.localPosition = new Vector3(-1.042f, -0.673f, -0.385f);
+        torchObject.transform.localRotation = Quaternion.Euler(-64.679f, 119.838f, 605.537f);
+        hasPickedUpTorch = true;
+        torchLight.SetActive(false);
+        torchSlotOFF.SetActive(false);
+        torchSlotON.SetActive(true);
+
+        torchSlider.gameObject.SetActive(true);
+
+        torchHintText1.gameObject.SetActive(true); 
+    }
+
+    void TurnOnTorch()
+    {
+        torchLight.SetActive(true);
+        isTorchOn = true;
+        torchSlotON.SetActive(true);
+        torchSlotOFF.SetActive(false);
+
+        torchTimer = torchDuration;
+        torchSlider.maxValue = torchDuration;
+        torchSlider.value = torchDuration;
+    }
+
+    void TurnOffTorch()
+    {
+        torchLight.SetActive(false);
+        isTorchOn = false;
+        torchSlotON.SetActive(false);
+        torchSlotOFF.SetActive(true);
+
+        StartCooldown();
+    }
+
+    void HandleTorchUsage()
+    {
+        torchTimer -= Time.deltaTime;
+        torchSlider.value = torchTimer;
+
+        if (torchTimer <= 0)
+        {
+            TurnOffTorch();
+        }
+    }
+
+    void StartCooldown()
+    {
+        isInCooldown = true;
+        cooldownTimer = 0f;
+        torchSlider.maxValue = cooldownDuration;
+        torchSlider.value = cooldownTimer;
+    }
+
+    void HandleCooldown()
+    {
+        cooldownTimer += Time.deltaTime;
+        torchSlider.value = cooldownTimer;
+
+        if (cooldownTimer >= cooldownDuration)
+        {
+            isInCooldown = false;
+            torchSlider.value = torchDuration;
         }
     }
 }
